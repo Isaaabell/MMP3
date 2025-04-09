5c8b83e1773a9633cf71636d9c00e0257e1a3bc1
Nicolas van Renen <fhs49246@fh-salzburg.ac.at>
2025-04-09 15:17:19 +0200

    new playermovement with new input system

--- MMP3/Assets/#CONTENT/Scripts/Player/MovementPlayer.cs
+++ MMP3/Assets/#CONTENT/Scripts/Player/MovementPlayer.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float lookSensitivity = 2f;
+    public Transform cameraTransform;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private float cameraPitch = 0f;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        // Move based on input
+        Vector3 move = transform.forward * moveInput.y + transform.right * moveInput.x;
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
     }
+
+    private void LateUpdate()
+    {
+        // Rotate player horizontally
+        transform.Rotate(Vector3.up * lookInput.x * lookSensitivity);
+
+        // Rotate camera vertically
+        cameraPitch -= lookInput.y * lookSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -80f, 80f);
+
+        if (cameraTransform)
+        {
+            cameraTransform.localEulerAngles = new Vector3(cameraPitch, 0f, 0f);
+        }
+    }
+
+    // Input System Actions
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+    
 }
