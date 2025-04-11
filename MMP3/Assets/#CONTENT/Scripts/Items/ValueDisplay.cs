622f2e23fc5b15d5edd816d43bb1397e1bea235e
Isabella Schönerer <ischoenerer.mmt-b2022@fh-salzburg.ac.at>
2025-04-11 12:51:22 +0200

    Level 1 changed

--- MMP3/Assets/#CONTENT/Scripts/Items/ValueDisplay.cs
+++ MMP3/Assets/#CONTENT/Scripts/Items/ValueDisplay.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.VersionControl;
-using TMPro.EditorUtilities;
 
 public class ValueDisplay : MonoBehaviour
 {
